660eca55d392e8dcd1663dc9d83aa7c424de2e13
david.jottrand <Da1018jo>
2020-01-17 16:54:09 +0100

    Initial commit

---
+++ src/main/java/dj/aurelia/aureliadiscovery/AureliaDiscoveryApplication.java
@@ -0,0 +1,18 @@
+package dj.aurelia.aureliadiscovery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AureliaDiscoveryApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(AureliaDiscoveryApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(AureliaDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dj/aurelia/aureliadiscovery/AureliaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dj.aurelia.aureliadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureliaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
